8581f1d34c17ed1b24dc0e033d9467ece7e9751f
syeongk <syeongggg@gmail.com>
2024-12-05 13:59:38 +0900

    First commit in dev branch

--- src/mygit/MyClass.java
+++ src/mygit/MyClass.java
@@ -4,5 +4,8 @@ public class MyClass {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello world! 2nd version");
+
+        YourClass your = new YourClass();
+        your.hello("everyone!");
     }
 }
