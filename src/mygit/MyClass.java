9c3f7ba8e250f6a2d2c0218beac02cd4ec4c2c75
syeongk <syeongggg@gmail.com>
2024-12-05 13:50:27 +0900

    2nd version

--- src/mygit/MyClass.java
+++ src/mygit/MyClass.java
@@ -3,5 +3,6 @@ package mygit;
 public class MyClass {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world! 2nd version");
     }
 }
